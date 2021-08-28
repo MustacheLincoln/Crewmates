ba25125c60258a36e1a00527880509339173dc71
MustacheLincoln <MustacheLincoln@gmail.com>
2021-08-28 15:57:35 -0500

    Added Camera Edge Panning

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -12,13 +12,26 @@ namespace Crewmates
         private float rotateSpeed = 5;
         private void Update()
         {
-            transform.localPosition -= new Vector3(Input.GetAxis("Mouse ScrollWheel") * zoomSpeed, 0,Input.GetAxis("Mouse ScrollWheel")*zoomSpeed);
+            transform.localPosition -= new Vector3(Input.GetAxis("Mouse ScrollWheel") * zoomSpeed * Time.deltaTime, 0, Input.GetAxis("Mouse ScrollWheel") * zoomSpeed * Time.deltaTime);
             if (transform.localPosition.z < 0)
             {
-                transform.localPosition += new Vector3(Input.GetAxis("Mouse ScrollWheel") * zoomSpeed, 0, Input.GetAxis("Mouse ScrollWheel") * zoomSpeed);
+                transform.localPosition += new Vector3(Input.GetAxis("Mouse ScrollWheel") * zoomSpeed * Time.deltaTime, 0, Input.GetAxis("Mouse ScrollWheel") * zoomSpeed * Time.deltaTime);
             }
             if (Input.GetMouseButton(2))
-                camFocus.transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * rotateSpeed, 0);
+                camFocus.transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime, 0);
+
+            float edgeSize = 8;
+            float zoomAmount = 25;
+            float rotateAmount = 50;
+            if (Input.mousePosition.x > Screen.width - edgeSize)
+                camFocus.transform.rotation *= Quaternion.Euler(0, -rotateAmount * Time.deltaTime, 0);
+            if (Input.mousePosition.x < edgeSize)
+                camFocus.transform.rotation *= Quaternion.Euler(0, rotateAmount * Time.deltaTime, 0);
+            if (Input.mousePosition.y > Screen.height - edgeSize)
+                if (transform.localPosition.z > 0)
+                    transform.localPosition -= new Vector3(zoomAmount * Time.deltaTime, 0, zoomAmount * Time.deltaTime);
+            if (Input.mousePosition.y < edgeSize)
+                transform.localPosition += new Vector3(zoomAmount * Time.deltaTime, 0, zoomAmount * Time.deltaTime);
         }
     }
 

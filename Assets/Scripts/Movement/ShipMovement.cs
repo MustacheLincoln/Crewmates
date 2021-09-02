c59be0ffbfe93bbbafc1a5369b6bb4ecb3aedb88
MustacheLincoln <MustacheLincoln@gmail.com>
2021-09-01 20:18:52 -0500

    Added Some Dumb Ship Avoidance

--- Assets/Scripts/Movement/ShipMovement.cs
+++ Assets/Scripts/Movement/ShipMovement.cs
@@ -29,8 +29,18 @@ namespace Crewmates
                     speed += 1 * Time.deltaTime;
                 transform.position += transform.forward * speed * Time.deltaTime;
 
-                Vector3 relativePos = (target.position + Vector3.right * 25) - transform.position;
+                Vector3 relativePos = target.position - transform.position;
                 Quaternion toRotation = Quaternion.LookRotation(relativePos);
+
+
+                RaycastHit hit;
+                // Does the ray intersect any objects excluding the player layer
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 250))
+                {
+                    toRotation = Quaternion.LookRotation(-relativePos);
+                }
+
+
                 transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, turnSpeed * (speed / 10) * Time.deltaTime);
             }
         }
